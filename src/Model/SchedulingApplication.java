1e6dab1feae6f068c0429ebc6131d707ac6e8be3
Eric Matelyan <eric.matelyan@gmail.com>
2021-07-20 12:47:55 -0400

    Pull rows from database using ResultSet object

--- src/Model/SchedulingApplication.java
+++ src/Model/SchedulingApplication.java
@@ -6,7 +6,16 @@
 package Model;
 
 import Utilities.DBConnection;
+import Utilities.DBQuery;
 import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -24,6 +33,26 @@ public class SchedulingApplication extends Application {
      */
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         DBConnection.startConnection();
+        
+        Connection conn = DBConnection.getConnection();
+        DBQuery.setStatement(conn);
+        Statement statement = DBQuery.getStatement();
+        
+        String selectStatement = "SELECT * FROM countries;";
+        statement.execute(selectStatement);
+        ResultSet resultSet = statement.getResultSet();
+        
+        while(resultSet.next()) {
+            int countryID = resultSet.getInt("Country_ID");
+            String countryName = resultSet.getString("Country");
+            LocalDate date = resultSet.getDate("Create_Date").toLocalDate();
+            LocalTime time = resultSet.getTime("Create_Date").toLocalTime();
+            String createdBy = resultSet.getString("Created_By");
+            LocalDateTime lastUpdate = resultSet.getTimestamp("Last_Update").toLocalDateTime();
+            
+            System.out.println(countryID + " | " + countryName + " | " + date + " | " + time + " | " + createdBy + " | " + lastUpdate);
+        }
+        
         launch(args);
         DBConnection.closeConnection();
     }
