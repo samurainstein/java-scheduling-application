42fc61b945f97f71d22066c6a84ba92871efec8e
Eric Matelyan <eric.matelyan@gmail.com>
2021-07-22 19:48:26 -0400

    Add Javadoc comments to user class

--- src/Model/User.java
+++ src/Model/User.java
@@ -12,39 +12,78 @@ package Model;
 public class User {
     
     //Members
+    /**
+     * This variable holds the user's userID. 
+     */
     private int userID;
     
+    /**
+     * This variable holds the user's username. 
+     */
     private String username;
     
+    /**
+     * This variable holds the user's password. 
+     */
     private String password;
 
     //Methods
+    /**
+     * This is the constructor for making a user object. 
+     * @param userID The userID to be assigned to the user object
+     * @param username The username to be assigned to the user object
+     * @param password The password to be assigned to the user object
+     */
     public User(int userID, String username, String password) {
         this.userID = userID;
         this.username = username;
         this.password = password;
     }
     
+    /**
+     * This method assigns a userID. 
+     * @param userID The user ID to be assigned
+     */
     public void setUserID(int userID) {
         this.userID = userID;
     }
     
+    /**
+     * This method assigns a username. 
+     * @param userID The username to be assigned
+     */
     public void setUsername(String username) {
         this.username = username;
     }
     
+    /**
+     * This method assigns a password. 
+     * @param userID The password to be assigned
+     */
     public void setPassword(String password) {
         this.password = password;
     }
     
+    /**
+     * This method returns a user ID. 
+     * @return Returns the user's ID
+     */
     public int getUserID() {
         return userID;
     }
     
+    /**
+     * This method returns a usernname. 
+     * @return Returns the user's username
+     */
     public String getUsername() {
         return username;
     }
     
+    /**
+     * This method returns a password. 
+     * @return Returns the user's password
+     */
     public String getPassword() {
         return password;
     }
