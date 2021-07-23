20ef52d9e866e6d3cf34fd1a718581ee18307634
Eric Matelyan <eric.matelyan@gmail.com>
2021-07-23 18:06:50 -0400

    Work on Login button

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -5,9 +5,22 @@
  */
 package Controller;
 
+import DAO.UserDAO;
+import Utilities.DBConnection;
+import Utilities.DBQuery;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,6 +29,11 @@ import javafx.fxml.Initializable;
  */
 public class LoginController implements Initializable {
 
+    @FXML
+    private TextField userNameTF;
+    @FXML
+    private TextField passwordTF;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +41,16 @@ public class LoginController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void onLogin(ActionEvent event) throws SQLException {
+        String username = userNameTF.getText();
+        String password = passwordTF.getText();
+        Connection conn = DBConnection.getConnection();
+        String sqlStatement = "SELECT * FROM users "
+                            + "WHERE User_Name = '" + username + "'"
+                            + "AND Password = '" + password + "';";
+        //UserDAO.selectUser(sqlStatement);
+    }
     
 }
