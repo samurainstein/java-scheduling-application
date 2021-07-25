f371c1c578ccc3441c5ce2e5a294cadfec788308
Eric Matelyan <eric.matelyan@gmail.com>
2021-07-25 11:36:35 -0400

    Create CustomerDAO class and start on methods for querying the DB

---
+++ src/DAO/CustomerDAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Model.Customer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author Eric
+ */
+public abstract class CustomerDAO {
+    //Methods
+    public void selectCustomers() {
+        
+    }
+}
