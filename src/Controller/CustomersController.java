c87b80443635f3d949e86b743bd2c199113ec0f1
Eric Matelyan <eric.matelyan@gmail.com>
2021-07-28 21:24:27 -0400

    Add Delete button and alerts

--- src/Controller/CustomersController.java
+++ src/Controller/CustomersController.java
@@ -10,14 +10,25 @@ import DAO.CustomerDAO;
 import Model.Country;
 import Model.Customer;
 import Model.Data;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -55,8 +66,73 @@ public class CustomersController implements Initializable {
         custPostalCol.setCellValueFactory(new PropertyValueFactory<>("PostalCode"));
         custDivisionCol.setCellValueFactory(new PropertyValueFactory<>("Division"));
         custCountryCol.setCellValueFactory(new PropertyValueFactory<>("Country"));
+        custTable.getItems().clear();
+        Data.clearCustomers();
         CustomerDAO.selectCustomers();
         custTable.setItems(Data.getAllCustomers());
     }    
+
+    @FXML
+    private void onAddCustomer(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/CustomerAdd.fxml"));
+        Stage stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Add Customer");
+        stage.show();
+    }
+
+    @FXML
+    private void onDelete(ActionEvent event) {
+        Customer customer = custTable.getSelectionModel().getSelectedItem();
+        int customerID = customer.getCustomerID();
+        if (customer == null){
+                JOptionPane.showMessageDialog(null, "Please Select a Customer");
+                return;
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to delete this customer?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if(result.isPresent() && result.get() == ButtonType.OK) { 
+                CustomerDAO.deleteCustomer(customerID);
+                //FIX THIS: How to refresh the table after delete?
+                //Data.clearCustomers();
+                //CustomerDAO.selectCustomers();
+                custTable.setItems(Data.getAllCustomers());
+                JOptionPane.showMessageDialog(null, "Selected customer was deleted");
+            }
+        //else {
+        //    JOptionPane.showMessageDialog(null, "No parts were deleted");
+        //}          
+        }
+        /*
+        if (customer == null){
+            Alert invalidAlert = new Alert(Alert.AlertType.ERROR);
+            invalidAlert.setTitle("Invalid Selection");
+            invalidAlert.setContentText("Please select a customer");
+            invalidAlert.showAndWait();
+            return;
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to delete this customer?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if(result.isPresent() && result.get() == ButtonType.OK) { 
+                CustomerDAO.deleteCustomer(customerID);
+                Alert confirmAlert = new Alert(Alert.AlertType.INFORMATION);
+                confirmAlert.setTitle("Confirmation");
+                confirmAlert.setContentText("Customer was deleted");
+                confirmAlert.showAndWait();
+            }
+        }
+        
+            else  {
+            Alert confirmAlert = new Alert(Alert.AlertType.INFORMATION);
+            confirmAlert.setTitle("Confirmation");
+            confirmAlert.setContentText("Customer was deleted");
+            confirmAlert.showAndWait();
+            }         
+        }*/
+        
+    }
     
 }
