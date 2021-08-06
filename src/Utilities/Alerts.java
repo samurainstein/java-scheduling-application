7b7fb3e3be8c0f9bf2141b1c5dfe2e49c55df62c
Eric Matelyan <eric.matelyan@gmail.com>
2021-08-06 13:30:17 -0400

    Create Alerts class to reuse alert generating code

---
+++ src/Utilities/Alerts.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilities;
+
+import javafx.scene.control.Alert;
+
+/**
+ *
+ * @author Eric
+ */
+public abstract class Alerts {
+    public static void appointmentNullAlert() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Invalid Selection");
+        alert.setContentText("Please select an appointment");
+        alert.showAndWait();
+    }
+    
+    public static void appointmentDeleteConfirm(int appointmentID) {
+        Alert confirmAlert = new Alert(Alert.AlertType.INFORMATION);
+            confirmAlert.setTitle("Confirmation");
+            confirmAlert.setContentText("Appointment ID: [" + appointmentID + "] was deleted");
+            confirmAlert.showAndWait();
+    }
+    
+    public static void appointmentUpcomingAlert() {
+        String popupTitle = "Appointment Notification";
+        String popupText = "You have an appointment in the next 15 minutes";
+        Alert popup = new Alert(Alert.AlertType.INFORMATION);
+        popup.setTitle(popupTitle);
+        popup.setContentText(popupText);
+        popup.showAndWait();
+    }
+    
+    public static void customerNullAlert() {
+        Alert invalidAlert = new Alert(Alert.AlertType.ERROR);
+        invalidAlert.setTitle("Invalid Selection");
+        invalidAlert.setContentText("Please select a customer");
+        invalidAlert.showAndWait();
+    }
+    
+    public static void customerDeleteConfirm() {
+        Alert confirmAlert = new Alert(Alert.AlertType.INFORMATION);
+        confirmAlert.setTitle("Confirmation");
+        confirmAlert.setContentText("Customer was deleted");
+        confirmAlert.showAndWait();
+    }
+    
+    public static void loginInvalid(String alertTitle, String alertText) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(alertTitle);
+        alert.setContentText(alertText);
+        alert.showAndWait();
+    }
+}
